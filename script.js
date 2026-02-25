41bd7774cf5119e30dca4db4117f661330aa17d8
Lennard Damer <114919150+lendamer@users.noreply.github.com>
2026-02-25 17:02:16 +0100

    Replace confirmation messages with new phrases

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Ok, du hast dich verklickt.",
+    "Junge was",
+    "dikka",
+    "STOOOPID",
+    "Ok du bist wirklich ein trottel",
+    "Essen geht auch auf mich :/",
+    "Ich bau nie wieder Lego mit dir zusammen",
+    "Sie macht einfach Anakin aus mir",
+    "OK JUNGE JETZT KLICK EINFACH JA",
+    "FUCK YOUU"
 ];
 
 let messageIndex = 0;
